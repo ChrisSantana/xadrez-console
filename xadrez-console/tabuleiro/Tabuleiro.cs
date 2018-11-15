813e864f3370932830e49f596093d2b4931a7aae
christiano <chris.santana.si@gmail.com>
2018-11-15 11:31:05 -0300

    adicionando objetos peca na classe tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -15,7 +15,8 @@
         }
 
         public void colocarPeca(Peca p, Posicao pos) {
-            //
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
         }
     }
 }
